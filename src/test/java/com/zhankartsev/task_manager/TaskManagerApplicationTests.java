927124aee4696a30b228a1b5446554114a5f49ed
Жан <saintleroy58@gmail.com>
2026-09-03 21:00:11 +0700

    Initial commit

---
+++ src/test/java/com/zhankartsev/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zhankartsev.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
